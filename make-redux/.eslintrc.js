feca0d9caaf7b27403b5d163a35240a035793ac1
liyang <yang.li@zxthealth.com>
2018-02-11 14:46:24 +0800

    init make-redux project

---
+++ make-redux/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "globals": {
+      "it": true,
+  },
+  "rules": {
+      "react/prop-types": 0,
+      "no-console": 0,
+      "no-use-before-define": 0,
+      "react/prefer-stateless-function": 0,
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "es6": true
+  },
+  plugins: [
+    "react",
+    "jsx-a11y",
+    "import"
+  ]
+};
