005c5ad53117b026ec89e0e6c753905b9479edb1
liyang <yang.li@zxthealth.com>
2018-02-26 15:20:10 +0800

    redux step 1

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue',
+  },
+};
+
+function dispatch(action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text;
+      break;
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color;
+      break;
+    default:
+      break;
+  }
+}
+
+function renderTitle(title) {
+  const titleDOM = document.getElementById('title');
+  titleDOM.innerHTML = title.text;
+  titleDOM.style.color = title.color;
+}
+
+function renderContent(content) {
+  const contentDOM = document.getElementById('content');
+  contentDOM.innerHTML = content.text;
+  contentDOM.style.color = content.color;
+}
+
+function renderApp(appStates) {
+  renderTitle(appStates.title);
+  renderContent(appStates.content);
+}
+
+dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' });
+dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' });
+renderApp(appState);
+
 registerServiceWorker();
